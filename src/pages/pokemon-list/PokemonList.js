6e69747a4f1ab6a2186ece332cd2edff3f535fe2
David Ruiz <dr.villota@uniandes.edu.co>
2022-05-26 18:36:32 -0500

    Chart

--- src/pages/pokemon-list/PokemonList.js
+++ src/pages/pokemon-list/PokemonList.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import './PokemonList.scss';
 import { FormattedNumber } from "react-intl";
 import { FormattedMessage } from "react-intl";
+import BarChart from "./../../components/bar-chart";
+import Pie from "./../../components/pie-chart";
 
 export const PokemonList = () => {
   //URLs data
   const urlES =
-    "https://gist.githubusercontent.com/jhonatan89/e379fadf8ed0f5381a2d8f8f3dea90c3/raw";
+    "https://gist.githubusercontent.com/jhonatan89/e379fadf8ed0f5381a2d8f8f3dea90c3/raw/f8357c439bbb7b4bd3dc6e8807c52105fb137ec6/pokemon-es.json";
   const urlEN =
-    "";
+    "https://gist.githubusercontent.com/jhonatan89/2089276d3ce0faceff8e55fc3459b818/raw/30ee1a77b3e328108faaaa9aaac6f2ddaa3d3711/pokemons-en.json";
   const [pokemons, setPokemons] = useState([]);
 
   //Get data
@@ -31,10 +33,12 @@ export const PokemonList = () => {
   const fetchPokemons = async () => {
     const respES = await fetch(urlES);
     const datpES = await respES.json();
+    const respEN = await fetch(urlEN);
+    const datpEN = await respEN.json();
     //console.log(dataES);
     var pokemons = null;
-    pokemons = datpES?.map((resp) => {
-        
+    if(getLanguage()){
+      pokemons = datpEN?.map((resp) => {
         return {
           id: resp.id,
           thumbnailImage: resp.ThumbnailImage,
@@ -45,8 +49,20 @@ export const PokemonList = () => {
           type: resp.type,
         };
       });
-    
-   
+    }
+    else{
+      pokemons = datpES?.map((resp) => {
+        return {
+          id: resp.id,
+          thumbnailImage: resp.ThumbnailImage,
+          name: resp.name,
+          description: resp.description,
+          height: resp.height,
+          weight: resp.weight,
+          type: resp.type,
+        };
+      });
+    }
     setPokemons(pokemons);
     localStorage.setItem("pokemons", JSON.stringify(pokemons));
   };
@@ -54,17 +70,17 @@ export const PokemonList = () => {
   return (
     <>
       <div className='pokemon-container'>
-        <h1>Most wanted pokemons</h1>
+        <h1><FormattedMessage id="Tittle"/></h1>
         <table className='table'>
           <thead>
             <tr>
               <th scope='col'>#</th>
               <th scope='col'><FormattedMessage id="Image"/></th>
-              <th scope='col'>Name</th>
-              <th scope='col'>Description</th>
-              <th scope='col'>Height</th>
-              <th scope='col'>Weight</th>
-              <th scope='col'>Type</th>
+              <th scope='col'><FormattedMessage id="Name"/></th>
+              <th scope='col'><FormattedMessage id="Description"/></th>
+              <th scope='col'><FormattedMessage id="Height"/></th>
+              <th scope='col'><FormattedMessage id="Weight"/></th>
+              <th scope='col'><FormattedMessage id="Type"/></th>
             </tr>
           </thead>
           <tbody>
@@ -81,6 +97,9 @@ export const PokemonList = () => {
           ))}  
           </tbody>
         </table>
+        <div>
+          <Pie data={pokemons}/>
+        </div>
       </div>
     </>
   );
